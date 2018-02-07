90beadb3a3323c0ca95cf2c8bbdaafaca265a238
Jubril Oyesiji <jubril.a.oyesiji@cbp.dhs.gov>
2018-02-07 11:03:19 -0500

    added assets

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,17 +8,19 @@ import { PhotoListComponent } from './photo-list/photo-list.component';
 
 import {PhotoService} from './services/photo.service';
 import { SidePanelComponent } from './side-panel/side-panel.component';
-
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { routing } from './app.routing';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     PhotoListComponent,
-    SidePanelComponent
+    SidePanelComponent,
+    NavBarComponent
   ],
   imports: [
-    BrowserModule, HttpModule
+    BrowserModule, HttpModule, routing
   ],
   providers: [PhotoService],
   bootstrap: [AppComponent]
