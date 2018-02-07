7912b9effdd0bf1e8f944c2162b0410683a785d0
Jubril Oyesiji <jubril.a.oyesiji@cbp.dhs.gov>
2018-02-07 10:23:09 -0500

    made changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -18,7 +18,7 @@ import { SidePanelComponent } from './side-panel/side-panel.component';
     SidePanelComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, HttpModule
   ],
   providers: [PhotoService],
   bootstrap: [AppComponent]
