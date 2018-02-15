f93f7453b6323135e4668dec0acba0f2392f648a
jubriloyesiji <jubriloyesiji@10.0.1.11>
2018-02-15 17:57:05 -0500

    made changes

--- src/app/models/photo.ts
+++ src/app/models/photo.ts
@@ -1,5 +1,5 @@
 import {Comment} from './comment';
-import {User} from './User';
+import {User} from './user';
 
 export class Photo {
   public photoId: number;
--- src/app/services/add-photo.service.ts
+++ src/app/services/add-photo.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import {Http, Headers} from '@angular/http';
-import {Photo} from '../models/Photo';
+import {Photo} from '../models/photo';
 import {Observable} from 'rxjs/Observable';
 
 
